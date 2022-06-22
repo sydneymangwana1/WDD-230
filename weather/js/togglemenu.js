8d41e818ed5ad26f51c267bf0af20674781914eb
Sydney Mangwana <man21063@byui.edu>
2022-06-22 05:55:31 +0200

    commit

---
+++ weather/js/togglemenu.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+	document.getElementById("menu-primary").classList.toggle("open")
+	document.getElementById("harmburger-button").classList.toggle("open")
+}
+
+const x = document.getElementById("harmburger-button")
+x.onclick = toggleMenu;
