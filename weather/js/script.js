a232a42917d7a218c3e7de7a053d6d9db7a2ec47
Sydney Mangwana <man21063@byui.edu>
2022-06-22 06:26:47 +0200

    commit

---
+++ weather/js/script.js
@@ -0,0 +1,13 @@
+let d = new Date();
+const days = ["Sun", "Mon", "Tuey", "Wed", "Thu", "Fri", "Sat"];
+let day = days[d.getDay()];
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[d.getMonth()];
+
+let fullyear = d.getFullYear()
+
+let t = d.getDate();
+
+let fulldate =  day + ", " + t + " " + month + " " + fullyear; 
+document.getElementById("current-date").textContent = fulldate;
